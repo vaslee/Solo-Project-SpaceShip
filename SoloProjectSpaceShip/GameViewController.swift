2b6a302ffd4e1d279daf7e9fa6ae7a07d1a9f0c8
vaslee <ltx0503@gmail.com>
2019-02-07 16:08:09 -0500

    background, my ship, and bullet set up, fixed the bullet image

--- SoloProjectSpaceShip/GameViewController.swift
+++ SoloProjectSpaceShip/GameViewController.swift
@@ -8,38 +8,26 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        let scene = GameScene(size: view.bounds.size)
             
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
+                scene.scaleMode = .aspectFill
                 
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
                 if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
+                    view.presentScene(scene)
                     
                     view.ignoresSiblingOrder = true
                     
                     view.showsFPS = true
                     view.showsNodeCount = true
                 }
-            }
-        }
+           
+        
     }
 
     override var shouldAutorotate: Bool {
